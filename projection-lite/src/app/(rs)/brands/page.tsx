9e02a1d39e9325bcb813fbae2b3694eb8bbd8695
Christopher Velasquez <Christopher.Velasquez@proton.me>
2025-04-16 08:48:55 -0700

    initial repository creation with base pages and a sticky header

---
+++ projection-lite/src/app/(rs)/brands/page.tsx
@@ -0,0 +1,5 @@
+export default function Brands() {
+    return (
+        <h2>Brand Page</h2>
+    )
+}
---
+++ projection-lite/src/app/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <div className="bg-black bg-[url(/images/home-img-bg.jpeg)] bg-cover bg-center">
+      <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
+
+        <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
+          <h1 className="text-4xl font-bold">Vintage Planner<br/>Plan Volumes</h1>
+          <p>Flavor text that is an hourly callout in example</p>
+          <Link href="tel:5555555555"
+          className="hover:underline">
+            555-555-5555
+          </Link>
+        </div>
+
+      </main>
+
+    </div>
+  );
+}
